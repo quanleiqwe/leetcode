87c582e41e3b37035f7a294fa7a7c8134b203609
quanlei <1451894473@qq.com>
2020-03-07 17:44:43 +0800

    add

---
+++ src/com/quan/linked/Demo21.java
@@ -0,0 +1,17 @@
+package com.quan.linked;
+
+public class Demo21 {
+
+    public class ListNode {
+        int val;
+        Demo2.ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    }
+}
